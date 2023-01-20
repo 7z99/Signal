fc3ed69c2a4b974439ba30ab4c3d84ca8713544a
7z99 <104286985+7z99@users.noreply.github.com>
2023-01-19 22:47:27 -0400

    Implement method signal:Once(), use task.spawn
    Implemented signal:Once method, switched from coroutine.resume to task.spawn because the former caused infinite yielding issue with remote (and possibly but not 100% sure) bindable functions.

--- Signal.lua
+++ Signal.lua
@@ -6,50 +6,67 @@ connection.__index = connection
 
 -- connection methods --
 
+local function assert(condition, message: any?, level)
+	return (condition or error(message or 'Assertion failed!', (level or 1) + 2)) :: typeof(condition)
+end
+
 function connection:Disconnect()
-	assert(self:IsConnection(), 'Attempt to call member function Connection.Disconnect on a non-connection value')
-	for k, v in ipairs(self.orgSelf.Connections) do
+	assert(not self._dead, 'Connection is dead!')
+	assert(self.IsConnection :: boolean, 'Attempt to call member function Connection.Disconnect on a non-connection value')
+	if not self.Connected then
+		return
+	end
+	for i, v in ipairs(self.orgSelf._connections) do
 		if v == self then
-			table.remove(self.orgSelf.Connections, k)
+			table.remove(self.orgSelf._connections, i)
 		end
 	end
 	self.Connected = false
 	setmetatable(self, nil)
 end
-connection.disconnect = connection.Disconnect
 
-function connection:IsConnection()
-	return true
-end
+connection.IsConnection = true
 
 -- signal methods --
-
-function signal:IsSignal()
-	return true
-end
 
 function signal.new()
 	return setmetatable({
 		['_connections'] = {};
+		['_once'] = {};
 		['_yields'] = {};
 	}, signal)
 end
 
 function signal:Fire(...)
-	assert(self:IsSignal(), 'Attempt to call member function Signal.Fire on a non-signal value!')
-	for _, v in ipairs(self._connections) do
-		coroutine.wrap(v.func)(...)
+	assert(not self._dead, 'Signal is dead!')
+	assert(self.IsSignal, 'Attempt to call member function Signal.Fire on a non-signal value!')
+	if self._dead then
+		return
+	end
+	for i, v in ipairs(self._connections) do
+		task.spawn(v.func, ...)
 	end
 
-	for k, v in ipairs(self._yields) do
-		coroutine.resume(v, ...)
-		table.remove(self._yields, k)
+	for i, v in ipairs(self._yields) do
+		if coroutine.status(v) == 'suspended' then
+			task.spawn(v, ...)
+		end
+	end
+	
+	table.clear(self._yields)
+	
+	for i,v in ipairs(self._once) do
+		task.spawn(v.func, ...)
+		if not v._dead then
+			v:Disconnect()
+			table.remove(self._once, v)
+		end
 	end
 end
-signal.fire = signal.Fire
 
 function signal:Connect(func)
-	assert(self:IsSignal(), 'Attempt to call member function Signal.Connect on a non-signal value!')
+	assert(not self._dead, 'Signal is dead!')
+	assert(self.IsSignal, 'Attempt to call member function Signal.Connect on a non-signal value!')
 	local connection = setmetatable({
 		func = func;
 		orgSelf = self;
@@ -59,27 +76,46 @@ function signal:Connect(func)
 	table.insert(self._connections, connection)
 	return connection
 end
-signal.connect = signal.Connect
 
 function signal:Wait()
-	assert(self:IsSignal(), 'Attempt to call member function Signal.Wait on a non-signal value!')
+	assert(not self._dead, 'Signal is dead!')
+	assert(self.IsSignal, 'Attempt to call member function Signal.Wait on a non-signal value!')
 	table.insert(self._yields, coroutine.running())
 	return coroutine.yield()
 end
-signal.wait = signal.Wait
 
 function signal:Destroy()
-	assert(self:IsSignal(), 'Attempt to call member function Signal.Destroy on a non-signal value!')
-	
-	for _, v in ipairs(self._connections) do
+	assert(not self._dead, 'Signal is dead!')
+	assert(self.IsSignal, 'Attempt to call member function Signal.Destroy on a non-signal value!')
+
+	for i, v in ipairs(self._connections) do
 		v:Disconnect()
 	end
-	for _, v in ipairs(self._yields) do
-		coroutine.resume(v)
+	for i, v in ipairs(self._yields) do
+		if coroutine.status(v) == 'suspended' then
+			task.spawn(v)
+		end
 	end
+	table.clear(self._yields)
 
 	setmetatable(self, nil)
+
+	self._dead = true
 end
-signal.destroy = signal.Destroy
+
+function signal:Once(func)
+	assert(not self._dead, 'Signal is dead!')
+	assert(self.IsSignal, 'Attempt to call member function Signal.Connect on a non-signal value!')
+	local newConnection = setmetatable({
+		func = func;
+		orgSelf = self;
+		Connected = true
+	}, connection)
+	table.insert(self._once, connection)
+	return newConnection
+end
+signal.IsSignal = true
+
+export type connection = typeof(signal.new():Connect())
 
 return signal
